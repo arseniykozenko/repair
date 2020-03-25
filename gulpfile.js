0d58bbeff8c9eb0d485216d42b4bb9c4e4c60a2e
Arseniy Kozenko <arseniy.kozenko@yandex.ru>
2020-03-26 00:57:43 +0200

    Настройка gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,52 @@ var gulp = require('gulp');
 var cleanCss = require('gulp-clean-css');
 var htmlmin = require('gulp-htmlmin');
 var tinypng = require('gulp-tinypng-compress');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(done) {
+    gulp.src('./src/sass/style.sass')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+        errorLogToConsole: true,
+        outputStyle: 'compressed'
+    }))
+    .on('error', console.error.bind(console))
+    .pipe(autoprefixer({
+        cascade: false
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./src/css/'))
+    done();
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./src/sass/**/*', gulp.series('sass'));
+});
 
 gulp.task('minify-css', function(done) {
     return gulp.src('./src/css/*.css')
+    .pipe(sourcemaps.init())
     .pipe(cleanCss({
         compatibility: 'ie8'
     }))
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist/css/'))
     done();
 });
 
-gulp.task('move-js', function(done) {
+gulp.task('minify-js', function(done) {
     return gulp.src('./src/js/*.js')
+    .pipe(uglify())
+    .pipe(rename({
+        suffix: '.min'
+    }))
     .pipe(gulp.dest('dist/js/'))
     done();
 });
@@ -42,6 +76,6 @@ gulp.task('tinypng', function (done) {
     done();
 });
 
-gulp.task('default', gulp.series('minify-css', 'move-js', 'fonts', 'htmlmin', 'tinypng', function(done) {
+gulp.task('default', gulp.series('minify-css', 'minify-js', 'fonts', 'htmlmin', 'tinypng', function(done) {
     done();
 }));
